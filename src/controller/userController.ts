4e9969eeb705fedc3be52fa55b7b57e38465c76c
AsHwInSkV <ashwinsukumaran.kuyil@gmail.com>
2025-01-06 12:59:33 +0000

    Initial commit

---
+++ src/controller/userController.ts
@@ -0,0 +1,11 @@
+const adminlogin = function(){
+
+};
+const loginUser = function(){
+
+};
+const registerUser = function(){
+
+};
+
+export { adminlogin , loginUser, registerUser}
---
+++ src/routes/userRoute.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { adminlogin, loginUser, registerUser } from "../controller/userController";
+import { Router } from "express";
+
+const userRouter : Router = express.Router();
+
+
+userRouter.post('/register',registerUser);
+userRouter.post('login',loginUser);
+userRouter.post('admin',adminlogin);
+
+export default userRouter;
