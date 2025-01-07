6ed3891302e78b16640b10ea0740540a8d636763
Ashwin K V <ashwinsukumaran.kuyil@gmail.com>
2025-01-07 06:29:51 +0530

    JWT

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,4 +1,17 @@
-const adminlogin = function(){
+import jwt from "jsonwebtoken";
+import { Request, Response } from 'express';
+
+const jwtSecret = process.env.JWT_SECRET;
+
+if(!jwtSecret){
+    throw new Error("JWT_SECRET is not defined in the environment variables");
+}
+
+const createToken = (id : string | number) : string=>{
+    return jwt.sign({id},jwtSecret);
+};
+
+const adminlogin = function(req :Request,res : Response) : void{
 
 };
 const loginUser = function(){
