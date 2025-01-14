44c44829015e8abc08d4622ab21c825d048ad994
AsHwInSkV <ashwinsukumaran.kuyil@gmail.com>
2025-01-14 13:19:00 +0000

    Add Update Cart Feature

--- src/controller/cartController.ts
+++ src/controller/cartController.ts
@@ -26,4 +26,62 @@ const getUserCart = async (req:Request,res:Response):Promise<void>=>{
     }
 };
 
-export {getUserCart};
+const addCartdata = async (req:Request,res:Response):Promise<void>=>{
+    try{
+        const {UserId,ItemId,size} = req.body;
+        const userdata = await UserModel.findById(UserId);
+        const cartdata = userdata?.cartdata || {};
+       
+        if(cartdata && cartdata[ItemId]){
+            if(cartdata[ItemId][size]){
+                cartdata[ItemId][size]+=1;
+            }
+            else{
+                cartdata[ItemId][size]=1;
+            }
+        }
+        else{
+            cartdata[ItemId]={};
+            cartdata[ItemId][size]=1;
+        }
+        await UserModel.findByIdAndUpdate(UserId,{cartdata});
+        res.json({
+            sucess:true,
+            message:"Added to cart!"
+        })
+    }
+    catch(error:unknown){
+        const error_message=(error instanceof Error)?error.message:"An Unexpected Error Occured!";
+        console.log(error);
+        res.json({
+            sucess:false,
+            message:error_message
+        });
+    }
+}
+
+const updateCartdata = async (req:Request,res:Response):Promise<void>=>{
+    try{
+        const {UserId,ItemId,size,quantity}=req.body;
+        let userData = await UserModel.findById(UserId);
+        let cartdata = userData?.cartdata;
+        if(cartdata){
+            cartdata[ItemId][size]=quantity;
+        }
+        await UserModel.findByIdAndUpdate(UserId,{cartdata});
+        res.json({
+            sucess:true,
+            message:"Cart Updated"
+        })
+    }
+    catch(error:unknown){
+        const error_message = (error instanceof Error)?error.message : "An Unexpected Error Occured!";
+        console.log(error);
+        res.json({
+            sucess:false,
+            message:error_message
+        })
+    }
+}
+
+export {getUserCart,addCartdata,updateCartdata};
--- src/routes/cartRouter.ts
+++ src/routes/cartRouter.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authUser from '../middlewares/userAuth';
-import { getUserCart } from '../controller/cartController';
+import { addCartdata, getUserCart, updateCartdata } from '../controller/cartController';
 import { get } from 'http';
 
 const cartRoute = express.Router();
 
 cartRoute.post("/get",authUser,getUserCart);
+cartRoute.post("/add",authUser,addCartdata);
+cartRoute.post("/update",authUser,updateCartdata);
 
 export default cartRoute;
